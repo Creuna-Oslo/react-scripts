f2d08d488a513aa96971698259033a03a4c42e8d
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-06-08 18:17:21 +0200

    created node interface for rename and to-stateless

---
+++ index.js
@@ -0,0 +1,7 @@
+const rename = require('./source/rename');
+const toStateless = require('./source/to-stateless');
+
+module.exports = {
+  rename,
+  toStateless
+};
--- source/rename.js
+++ source/rename.js
@@ -12,118 +12,126 @@ const getConfigs = require('./utils/get-configs');
 const prompt = require('./utils/prompt');
 const renameJSXTransform = require('./transforms/rename-jsx');
 
-prompt(
-  {
-    componentName: {
-      text: 'Name of component',
-      value: process.argv[2]
+module.exports = function(componentName, newComponentName) {
+  prompt(
+    {
+      componentName: {
+        text: 'Name of component',
+        value: componentName
+      },
+      newComponentName: {
+        text: 'New name of component',
+        value: newComponentName
+      }
     },
-    newComponentName: {
-      text: 'New name of component',
-      value: process.argv[3]
+    ({ componentName, newComponentName }) => {
+      renameComponent(componentName, newComponentName);
     }
-  },
-  ({ componentName, newComponentName }) => {
-    renameComponent(componentName, newComponentName);
-  }
-);
+  );
 
-function renameComponent(componentName, newComponentName) {
-  getConfigs(({ prettierConfig, componentsPath }) => {
-    const pascalNewComponentName = kebabToPascal(newComponentName);
+  function renameComponent(componentName, newComponentName) {
+    getConfigs(({ prettierConfig, componentsPath }) => {
+      const pascalNewComponentName = kebabToPascal(newComponentName);
 
-    const indexFilename = 'index.js';
-    const jsxFilename = `${componentName}.jsx`;
-    const scssFilename = `${componentName}.scss`;
+      const indexFilename = 'index.js';
+      const jsxFilename = `${componentName}.jsx`;
+      const scssFilename = `${componentName}.scss`;
 
-    const folderPath = path.join(componentsPath, componentName);
-    const newFolderPath = path.join(componentsPath, newComponentName);
+      const folderPath = path.join(componentsPath, componentName);
+      const newFolderPath = path.join(componentsPath, newComponentName);
 
-    ensureComponentExists(folderPath, componentName);
-    ensureEmptyFolder(newFolderPath, newComponentName);
+      ensureComponentExists(folderPath, componentName);
+      ensureEmptyFolder(newFolderPath, newComponentName);
 
-    const jsxFileContent = fs.readFileSync(path.join(folderPath, jsxFilename), {
-      encoding: 'utf-8'
-    });
-
-    const newJsxFileContent = renameJSXTransform(
-      jsxFileContent,
-      componentName,
-      newComponentName
-    );
-
-    fs.writeFileSync(
-      path.join(folderPath, jsxFilename),
-      prettier.format(newJsxFileContent, prettierConfig)
-    );
-    console.log(`💾  ${chalk.blueBright(jsxFilename)} written`);
-
-    fs.writeFileSync(
-      path.join(folderPath, indexFilename),
-      prettier.format(
-        `import ${pascalNewComponentName} from './${newComponentName}';
-    
-    export default ${pascalNewComponentName};`,
-        prettierConfig
-      )
-    );
-    console.log(`💾  ${chalk.blueBright(indexFilename)} written`);
-
-    // Overwrite index.js file with new component name
-    const newJsxFilename = `${newComponentName}.jsx`;
-    fs.renameSync(
-      path.join(folderPath, jsxFilename),
-      path.join(folderPath, newJsxFilename)
-    );
-    console.log(
-      `💾  ${chalk.blueBright(jsxFilename)} renamed to ${chalk.blueBright(
-        newJsxFilename
-      )}`
-    );
-
-    // Rename scss file and class names if it exists
-    if (fs.existsSync(path.join(folderPath, scssFilename))) {
-      // Replace selectors
-      const scssFileContent = fs.readFileSync(
-        path.join(folderPath, scssFilename),
+      const jsxFileContent = fs.readFileSync(
+        path.join(folderPath, jsxFilename),
         {
           encoding: 'utf-8'
         }
       );
-      const scssRegex = new RegExp(`\\.${componentName}( |-)`, 'g');
-      const newScssFileContent = scssFileContent.replace(
-        scssRegex,
-        `.${newComponentName}$1`
+
+      const newJsxFileContent = renameJSXTransform(
+        jsxFileContent,
+        componentName,
+        newComponentName
       );
 
-      fs.writeFileSync(path.join(folderPath, scssFilename), newScssFileContent);
-      console.log(`💾  ${chalk.blueBright(scssFilename)} written`);
+      fs.writeFileSync(
+        path.join(folderPath, jsxFilename),
+        prettier.format(newJsxFileContent, prettierConfig)
+      );
+      console.log(`💾  ${chalk.blueBright(jsxFilename)} written`);
 
-      const newScssFilename = `${newComponentName}.scss`;
+      fs.writeFileSync(
+        path.join(folderPath, indexFilename),
+        prettier.format(
+          `import ${pascalNewComponentName} from './${newComponentName}';
+    
+    export default ${pascalNewComponentName};`,
+          prettierConfig
+        )
+      );
+      console.log(`💾  ${chalk.blueBright(indexFilename)} written`);
+
+      // Overwrite index.js file with new component name
+      const newJsxFilename = `${newComponentName}.jsx`;
       fs.renameSync(
-        path.join(folderPath, scssFilename),
-        path.join(folderPath, newScssFilename)
+        path.join(folderPath, jsxFilename),
+        path.join(folderPath, newJsxFilename)
       );
       console.log(
-        `💾  ${chalk.blueBright(scssFilename)} renamed to ${chalk.blueBright(
-          newScssFilename
+        `💾  ${chalk.blueBright(jsxFilename)} renamed to ${chalk.blueBright(
+          newJsxFilename
         )}`
       );
-    }
 
-    // Rename component folder
-    fs.rename(folderPath, newFolderPath, err => {
-      if (err) {
-        console.log(`👻  ${chalk.red('Error renaming folder')}`, err);
+      // Rename scss file and class names if it exists
+      if (fs.existsSync(path.join(folderPath, scssFilename))) {
+        // Replace selectors
+        const scssFileContent = fs.readFileSync(
+          path.join(folderPath, scssFilename),
+          {
+            encoding: 'utf-8'
+          }
+        );
+        const scssRegex = new RegExp(`\\.${componentName}( |-)`, 'g');
+        const newScssFileContent = scssFileContent.replace(
+          scssRegex,
+          `.${newComponentName}$1`
+        );
 
-        process.exit(1);
+        fs.writeFileSync(
+          path.join(folderPath, scssFilename),
+          newScssFileContent
+        );
+        console.log(`💾  ${chalk.blueBright(scssFilename)} written`);
+
+        const newScssFilename = `${newComponentName}.scss`;
+        fs.renameSync(
+          path.join(folderPath, scssFilename),
+          path.join(folderPath, newScssFilename)
+        );
+        console.log(
+          `💾  ${chalk.blueBright(scssFilename)} renamed to ${chalk.blueBright(
+            newScssFilename
+          )}`
+        );
       }
 
-      console.log(
-        `💾  folder ${chalk.blueBright(
-          componentName
-        )} renamed to ${chalk.blueBright(chalk.blueBright(newComponentName))}`
-      );
+      // Rename component folder
+      fs.rename(folderPath, newFolderPath, err => {
+        if (err) {
+          console.log(`👻  ${chalk.red('Error renaming folder')}`, err);
+
+          process.exit(1);
+        }
+
+        console.log(
+          `💾  folder ${chalk.blueBright(
+            componentName
+          )} renamed to ${chalk.blueBright(chalk.blueBright(newComponentName))}`
+        );
+      });
     });
-  });
-}
+  }
+};
