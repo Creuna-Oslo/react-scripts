1622ea92b4e76013a41420c906305cd97a60e1eb
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-06-09 15:38:16 +0200

    added bin commands

---
+++ bin/rename.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const rename = require('../source/rename');
+
+rename(process.argv[2], process.argv[3]);
---
+++ bin/to-stateless.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const toStateless = require('../source/to-stateless');
+
+toStateless(process.argv[2]);
