f70db55444b3dd98e0c5d9ee4a3c1b1298894cf2
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-06-10 00:51:12 +0200

    removed unused kebab-to-human util

--- source/utils/kebab-to-human.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(kebabString) {
-  const stringWithoutDashes = kebabString.replace(/-/g, ' ');
-  return (
-    stringWithoutDashes.charAt(0).toUpperCase() + stringWithoutDashes.slice(1)
-  );
-};
