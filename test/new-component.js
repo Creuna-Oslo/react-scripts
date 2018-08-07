96749cb8fe1a5be6577bd5f61e43e30ee32be456
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-08-07 19:20:21 +0200

    Adds test for new-component

---
+++ test/new-component.js
@@ -0,0 +1,37 @@
+const test = require('ava');
+const fs = require('fs');
+const path = require('path');
+const tempy = require('tempy');
+
+const newComponent = require('../source/new-component');
+
+const template = (t, pathOrName, componentsPathRelative, shouldBeStateful) => {
+  t.plan(2);
+
+  const componentName = path.basename(pathOrName, '.jsx');
+  const expectedFilesNames = [
+    `${componentName}.jsx`,
+    `${componentName}.scss`,
+    'index.js'
+  ];
+  const tempDir = tempy.directory();
+  const componentsPath = path.join(tempDir, componentsPathRelative);
+  const componentPath = path.join(componentsPath, pathOrName);
+
+  newComponent({
+    componentsPath,
+    pathOrName,
+    shouldBeStateful
+  }).then(() => {
+    t.snapshot(
+      fs.readFileSync(path.join(componentPath, `${componentName}.jsx`), 'utf-8')
+    );
+    t.deepEqual(fs.readdirSync(componentPath), expectedFilesNames);
+    t.end();
+  });
+};
+
+test.cb('Stateless', template, 'component', '', false);
+test.cb('Stateful', template, 'component', '', true);
+
+test.cb('Relative path', template, 'test/test/component', '');
