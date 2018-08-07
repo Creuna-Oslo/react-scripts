563a081fabd4cd10bda73b1dff9fdaf64174ea56
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-08-07 23:46:49 +0200

    Adds test for rename

---
+++ test/rename.js
@@ -0,0 +1,47 @@
+const test = require('ava');
+const fs = require('fs');
+const fsExtra = require('fs-extra');
+const path = require('path');
+const tempy = require('tempy');
+
+const rename = require('../source/rename');
+
+test.cb('Rename', t => {
+  t.plan(4);
+
+  const componentName = 'component-stateful';
+  const newComponentName = 'new-name';
+  const tempDir = tempy.directory();
+  const newComponentPath = path.join(tempDir, newComponentName);
+
+  fsExtra.copySync(
+    path.join(__dirname, '..', 'test-components', componentName),
+    path.join(tempDir, componentName)
+  );
+
+  const expectedFileNames = [
+    'index.js',
+    `${newComponentName}.jsx`,
+    `${newComponentName}.scss`
+  ];
+
+  rename({
+    componentsPath: tempDir,
+    newComponentName: newComponentName,
+    pathOrName: componentName
+  }).then(() => {
+    t.deepEqual(
+      expectedFileNames,
+      fs
+        .readdirSync(newComponentPath)
+        .filter(fileName => expectedFileNames.includes(fileName))
+    );
+    expectedFileNames.forEach(fileName => {
+      t.snapshot(
+        fs.readFileSync(path.join(newComponentPath, fileName), 'utf-8'),
+        fileName
+      );
+    });
+    t.end();
+  });
+});
