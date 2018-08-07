80e080cdfb1914bed5dfe4930f8141846aa9d26f
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-08-07 23:33:53 +0200

    Adds test for to-stateful

---
+++ test/to-stateful.js
@@ -0,0 +1,32 @@
+const test = require('ava');
+const fs = require('fs');
+const fsExtra = require('fs-extra');
+const path = require('path');
+const tempy = require('tempy');
+
+const toStateful = require('../source/to-stateful');
+
+test.cb('To stateful', t => {
+  t.plan(1);
+
+  const componentName = 'component-stateless';
+  const tempDir = tempy.directory();
+
+  fsExtra.copySync(
+    path.join(__dirname, '..', 'test-components', componentName),
+    path.join(tempDir, componentName)
+  );
+
+  toStateful({
+    componentsPath: tempDir,
+    pathOrName: componentName
+  }).then(() => {
+    t.snapshot(
+      fs.readFileSync(
+        path.join(tempDir, componentName, `${componentName}.jsx`),
+        'utf-8'
+      )
+    );
+    t.end();
+  });
+});
