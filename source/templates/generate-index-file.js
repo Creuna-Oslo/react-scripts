fa765b99c0077554d05cca295a38f635f64c122a
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-10-09 21:29:33 +0200

    Adds jsx file extension to import in generated index file

--- source/templates/generate-index-file.js
+++ source/templates/generate-index-file.js
@@ -4,7 +4,7 @@ const kebabToPascal = require('@creuna/utils/kebab-to-pascal').default;
 module.exports = function(componentName) {
   const pascalComponentName = kebabToPascal(componentName);
 
-  return `import ${pascalComponentName} from './${componentName}';
+  return `import ${pascalComponentName} from './${componentName}.jsx';
     
   export default ${pascalComponentName};`;
 };
