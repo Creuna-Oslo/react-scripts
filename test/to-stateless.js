52d77e52327bd35d2395352547cae62e09c32191
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-02-12 14:09:11 +0100

    Adds test for non-transformable stateful component

--- test/to-stateless.js
+++ test/to-stateless.js
@@ -7,10 +7,9 @@ const toStateless = require('../source/to-stateless');
 const eslintConfig = require('../.eslintrc.json');
 const { readFile, readFixture } = require('./helpers/read');
 
-const componentName = 'component-stateful';
-const fixturePath = path.resolve(__dirname, '..', 'fixtures', componentName);
-
-test('To stateless', async t => {
+test('Happy path', async t => {
+  const componentName = 'component-stateful';
+  const fixturePath = path.resolve(__dirname, '..', 'fixtures', componentName);
   const tempDir = tempy.directory();
   const filePath = path.join(tempDir, componentName, `${componentName}.jsx`);
   const expected = readFixture(path.join(componentName, 'transformed.jsx'));
@@ -20,4 +19,30 @@ test('To stateless', async t => {
   await toStateless({ eslintConfig, filePath });
 
   t.is(expected, readFile(filePath));
+});
+
+test('With not transformable component', async t => {
+  const componentName = 'component-stateful-no-transform';
+  const fixturePath = path.resolve(__dirname, '..', 'fixtures', componentName);
+  const tempDir = tempy.directory();
+  const filePath = path.join(tempDir, componentName, `${componentName}.jsx`);
+
+  fsExtra.copySync(fixturePath, path.join(tempDir, componentName));
+
+  const error = await t.throwsAsync(
+    toStateless({ eslintConfig, filePath }).catch(errorMessage => {
+      throw new Error(errorMessage);
+    })
+  );
+  t.is(
+    `Component can't be converted. Make sure that there is no:
+  • state or references to state
+  • class methods
+  • refs`,
+    // NOTE: the error message also contains a stack trace after the fourth line with is excluded here
+    error.message
+      .split('\n')
+      .slice(0, 4)
+      .join('\n')
+  );
 });
